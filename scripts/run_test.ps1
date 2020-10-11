ed93b22671510dac9ed6fc18ce98b52df8fc9436
Federico Antuña <federicoantuna@hotmail.com>
2020-10-11 14:16:33 -0300

    Code Coverage Fixes

---
+++ scripts/run_test.ps1
@@ -0,0 +1,38 @@
+# Parameters - Solution
+$SolutionFileName = "WAM.sln"
+$CoverageCoverletDir = ".coverage"
+$CoverageReportDir = ".coverage-report"
+
+# Parameters - Coverlet
+$DataCollectorFormat = "cobertura"
+$CoverletOutputFormat = "cobertura"
+$CoverletOutputExtension = ".xml"
+$Collect = "XPlat Code Coverage"
+$CoverageFileName = "coverage.$CoverletOutputFormat$CoverletOutputExtension"
+
+# Parameters - Report Generator
+$ReportTypes = "HTML;cobertura;"
+$HtmlReportIndexFileName = "index.html"
+
+# Calculated Parameters - Solution - CHANGE WITH EXTREME CAUTION
+$SolutionDir = (Get-Item $PSScriptRoot).Parent.FullName
+$Solution = [IO.Path]::Combine($SolutionDir, $SolutionFileName)
+
+# Calculated Parameters - Coverlet - CHANGE WITH EXTREME CAUTION
+$CoverageRunIdentifier = [GUID]::NewGuid().ToString()
+$CoverletOutput = [IO.Path]::Combine($SolutionDir, $CoverageCoverletDir, $CoverageRunIdentifier)
+
+# Build and Test
+dotnet.exe test $Solution --collect:"$Collect" --results-directory:"$CoverletOutput" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format="$DataCollectorFormat"
+
+# Calculated Parameters - Report Generator - CHANGE WITH EXTREME CAUTION
+$TargetDir = [IO.Path]::Combine($CoverageReportDir, $CoverageRunIdentifier)
+$ReportHtmlFile = [IO.Path]::Combine($SolutionDir, $CoverageReportDir, $CoverageRunIdentifier, $HtmlReportIndexFileName)
+$TestResultsDirs = Join-Path $CoverletOutput -ChildPath **\*
+$CoverageFiles = (Split-Path $TestResultsDirs -Resolve | ForEach-Object -Process {[IO.Path]::Combine($_, $CoverageFileName)}) -join ";"
+
+# Generate Report
+reportgenerator.exe "-reports:$CoverageFiles" "-targetdir:$TargetDir" "-reporttypes:$ReportTypes"
+
+# Open Report
+Invoke-Item $ReportHtmlFile
---
+++ scripts/run_test.sh
@@ -0,0 +1,37 @@
+# Parameters - Solution
+SOLUTION_FILE_NAME="WAM.sln"
+COVERAGE_COVERLET_DIR=".coverage"
+COVERAGE_REPORT_DIR=".coverage-report"
+
+# Parameters - Coverlet
+DATA_COLLECTOR_FORMAT="cobertura"
+COVERLET_OUTPUT_FORMAT="cobertura"
+COVERLET_OUTPUT_EXTENSION=".xml"
+COLLECT="XPlat Code Coverage"
+COVERAGE_FILE_NAME="coverage.$COVERLET_OUTPUT_FORMAT$COVERLET_OUTPUT_EXTENSION"
+
+# Parameters - Report Generator
+REPORT_TYPES="HTML;cobertura;"
+HTML_REPORT_INDEX_FILE_NAME="index.html"
+
+# Calculated Parameters - Solution - CHANGE WITH EXTREME CAUTION
+SOLUTION_DIR="$(dirname $(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd))"
+SOLUTION="$SOLUTION_DIR/$SOLUTION_FILE_NAME"
+
+# Calculated Parameters - Coverlet - CHANGE WITH EXTREME CAUTION
+COVERAGE_RUN_IDENTIFIER=$(uuidgen)
+COVERLET_OUTPUT="$SOLUTION_DIR/$COVERAGE_COVERLET_DIR/$COVERAGE_RUN_IDENTIFIER"
+
+# Build and Test
+dotnet test $SOLUTION --collect:"$COLLECT" --results-directory:"$COVERLET_OUTPUT" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format="$DATA_COLLECTOR_FORMAT"
+
+# Calculated Parameters - Report Generator - CHANGE WITH EXTREME CAUTION
+TARGET_DIR="$COVERAGE_REPORT_DIR/$COVERAGE_RUN_IDENTIFIER"
+REPORT_HTML_FILE="$SOLUTION_DIR/$COVERAGE_REPORT_DIR/$COVERAGE_RUN_IDENTIFIER/$HTML_REPORT_INDEX_FILE_NAME"
+COVERAGE_FILES=$(find $COVERLET_OUTPUT/**/* -maxdepth 1 | awk -vORS=";" '{ print $1 }' | sed "s/\;$/\n/")
+
+# Generate Report
+reportgenerator "-reports:$COVERAGE_FILES" "-targetdir:$TARGET_DIR" "-reporttypes:$REPORT_TYPES"
+
+# Open Report
+open $REPORT_HTML_FILE
