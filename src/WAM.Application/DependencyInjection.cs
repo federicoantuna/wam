1d0db9b8a5477e0c799d1d088178a476bef17b60
Federico Antuña <federicoantuna@hotmail.com>
2020-10-24 12:54:59 -0300

    Added dependency injection on Application and Infrastructure layers

---
+++ src/WAM.Application/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using WAM.Application.Common.Behaviours;
+
+namespace WAM.Application
+{
+    /// <summary>
+    /// Provides the Application Dependency Injection.
+    /// </summary>
+    // ExcludeFromCodeCoverage: There is no value in testing this and it is extremely hard to test.
+    [ExcludeFromCodeCoverage]
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Adds the corresponding services for the application layer.
+        /// </summary>
+        /// <param name="services">The services collection.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+
+            _ = services.AddAutoMapper(executingAssembly);
+            _ = services.AddMediatR(executingAssembly);
+            _ = services.AddMediatR(executingAssembly);
+
+            _ = services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            _ = services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+
+            return services;
+        }
+    }
+}
---
+++ src/WAM.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+using WAM.Application.Common.Interfaces;
+using WAM.Infrastructure.Persistence;
+using WAM.Infrastructure.Services;
+
+namespace WAM.Infrastructure
+{
+    [ExcludeFromCodeCoverage]
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            _ = services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
+                soa => soa.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)),
+                ServiceLifetime.Transient);
+
+            _ = services.AddTransient<IApplicationDbContext>(sp => sp.GetService<ApplicationDbContext>());
+
+            _ = services.AddScoped<IDomainEventService, DomainEventService>();
+
+            return services;
+        }
+    }
+}
